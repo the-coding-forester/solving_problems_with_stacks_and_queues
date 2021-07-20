9dec567aa74cec1eac9426b2f02eec324abe0e5d
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-20 11:08:00 -0400

    postfix evaluation function complete

--- src/postfix.js
+++ src/postfix.js
@@ -1,3 +1,40 @@
-const evaluate = (expression) => { };
+const Stack = require("./lib/stack");
+
+const evaluate = (expression) => {
+  // initialize an empty stack
+  const stack = new Stack();
+
+  // remove all spaces from the expression
+  expression = expression.replace(/\s/g, "");
+
+  let result;
+
+  // for each character in the expression
+  expression.split("").forEach((character) => {
+    // if the character is an operand push it unto the stack
+    if (!isNaN(character)) {
+      stack.push(parseInt(character))
+    } else {
+      let b = stack.pop();
+      let a = stack.pop();
+      switch (character) {
+        case '+':
+          result = a + b;
+          break;
+        case '-':
+          result = a - b;
+          break;
+        case '*':
+          result = a * b;
+          break;
+        case '/':
+          result = a / b;
+          break;
+      }
+      stack.push(result);
+    }
+  })
+  return stack.pop();
+};
 
 module.exports = evaluate;
