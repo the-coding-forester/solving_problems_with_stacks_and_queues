45e8f646929ac760dbb83a6a57573bb599c39804
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-20 13:01:24 -0400

    all tests passing

--- src/brackets.js
+++ src/brackets.js
@@ -1,5 +1,43 @@
 const Stack = require("./lib/stack");
 
-const match = (expression) => {};
+const match = (expression) => {
+  const stack = new Stack();
+
+  for (let index = 0, limit = expression.length; index < limit; index++) {
+    switch (expression[index]) {
+      case "(":
+        stack.push("(");
+        break;
+      case "{":
+        stack.push("{")
+        break;
+      case "[":
+        stack.push("[");
+        break;
+      case ")":
+        if (stack.top === null || stack.top.value !== "(") {
+          return false
+        } else {
+          stack.pop()
+        }
+        break;
+      case "}":
+        if (stack.top === null || stack.top.value !== "{") {
+          return false
+        } else {
+          stack.pop()
+        }
+        break;
+      case "]":
+        if (stack.top === null || stack.top.value !== "[") {
+          return false
+        } else {
+          stack.pop()
+        }
+        break;
+    }
+  }
+  return !stack.top;
+};
 
 module.exports = match;
